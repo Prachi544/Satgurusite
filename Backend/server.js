115f8998046df0add4779f618c0ff731968bc146
Prachi544 <prachivaishnav215@gmail.com>
2026-07-17 18:20:25 +0530

    initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,8 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set("trust proxy", 1);
+
 /* ---------------------------------------------------------
    Middleware
 --------------------------------------------------------- */
