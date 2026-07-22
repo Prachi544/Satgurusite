ed20f33b728e2afee059d3e0d28d305d802a4095
gautam vaishnav <gvc7018@gmail.com>
2026-07-22 13:54:21 +0530

    Change SMTP port from 587 to 465 for secure connection

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ const bookingLimiter = rateLimit({
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
+  port: 465,
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
